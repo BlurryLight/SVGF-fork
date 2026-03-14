32de21715101d888dc809ab9343ed0eba185ab07
BlurryLight <zhonghaoyu555@gmail.com>
2026-03-14 21:22:20 +0800

    add OpenGL debug callback and enable debug output in Window initialization

--- src/Window.cpp
+++ src/Window.cpp
@@ -6,6 +6,49 @@
 
 #include <iostream>
 
+
+void APIENTRY OpenGLDebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
+ GLsizei /*length*/, const GLchar* message, const void* /*userParam*/)
+{
+    const char* src = "UNKNOWN";
+    const char* tp = "UNKNOWN";
+    const char* sev = "UNKNOWN";
+
+    switch (source) {
+    case GL_DEBUG_SOURCE_API: src = "API"; break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM: src = "WINDOW_SYSTEM"; break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER: src = "SHADER_COMPILER"; break;
+    case GL_DEBUG_SOURCE_THIRD_PARTY: src = "THIRD_PARTY"; break;
+    case GL_DEBUG_SOURCE_APPLICATION: src = "APPLICATION"; break;
+    case GL_DEBUG_SOURCE_OTHER: src = "OTHER"; break;
+    }
+
+    switch (type) {
+    case GL_DEBUG_TYPE_ERROR: tp = "ERROR"; break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: tp = "DEPRECATED"; break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: tp = "UNDEFINED"; break;
+    case GL_DEBUG_TYPE_PORTABILITY: tp = "PORTABILITY"; break;
+    case GL_DEBUG_TYPE_PERFORMANCE: tp = "PERFORMANCE"; break;
+    case GL_DEBUG_TYPE_MARKER: tp = "MARKER"; break;
+    case GL_DEBUG_TYPE_PUSH_GROUP: tp = "PUSH_GROUP"; break;
+    case GL_DEBUG_TYPE_POP_GROUP: tp = "POP_GROUP"; break;
+    case GL_DEBUG_TYPE_OTHER: tp = "OTHER"; break;
+    }
+
+    switch (severity) {
+    case GL_DEBUG_SEVERITY_HIGH: sev = "HIGH"; break;
+    case GL_DEBUG_SEVERITY_MEDIUM: sev = "MEDIUM"; break;
+    case GL_DEBUG_SEVERITY_LOW: sev = "LOW"; break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION: sev = "NOTIFY"; break;
+    }
+
+    if ((type == GL_DEBUG_TYPE_ERROR || type == GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR)
+        || (severity == GL_DEBUG_SEVERITY_HIGH))
+    {
+        std::printf("[GL DEBUG][%s][%s][%s][id=%u] %s\n", src, tp, sev, id, message);
+    }
+}
+
 namespace gpupt
 {
 
@@ -59,12 +102,33 @@ window::window(uint32_t Width, uint32_t Height)
 
     glfwSetWindowUserPointer(this->Handle, (void*)this);
 
-    // int version = gladLoadGL(glfwGetProcAddress);
-    int version = gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+    // 这个API必须要glad生成的时候带上loader
+    int version = gladLoadGL(glfwGetProcAddress);
+#if !defined(NDEBUG)
+    gladInstallGLDebug();
+#endif
+
+
+    // int version = gladLoaderLoadGL((GLADloadproc) glfwGetProcAddress);
     if (version == 0) {
         printf("Failed to initialize OpenGL context\n");
         exit(0);
     }
+
+#if !defined(NDEBUG)
+    if (GLAD_GL_VERSION_4_3 || GLAD_GL_KHR_debug)
+    {
+        GLint flags =0;
+        glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+
+        if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+        {
+            glEnable(GL_DEBUG_OUTPUT);
+            glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+            glDebugMessageCallback(OpenGLDebugCallback, nullptr);
+        }
+    }
+#endif
 }
 
 
